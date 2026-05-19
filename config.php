24760f374650fd038a015649f9aedbd278e203f5
Michael VanDerwerker <michaelvanderwerkerllc@gmail.com>
2026-05-19 15:57:30 -0500

    Fixing the issue with the env inclusion.

--- config.php
+++ config.php
@@ -2,10 +2,12 @@
 
 return [
     'google' => [
+        'client_id'     => env('SYNCRA_GOOGLE_CLIENT_ID'),
+        'client_secret' => env('SYNCRA_GOOGLE_CLIENT_SECRET'),
         // URL endpoint for the OAuth from Google
         'url' => 'https://accounts.google.com/o/oauth2/v2/auth',
         // Route where the callback comes to
-        'redirect_uri' => 'http://localhost:8000/google/validate',
+        'redirect_uri' => env('SYNCRA_GOOGLE_REDIRECT_URI', 'http://localhost:8000/google/validate'),
         // Route where the token is exchanged or grabbed via the auth code
         'exchange_uri' => 'https://oauth2.googleapis.com/token',
         // List of all scopes that are allowed to be used within this application. Set this to NULL to allow all scopes
